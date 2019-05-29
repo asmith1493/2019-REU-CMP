ffe7b5fe12b9dff72b40508673b4f01c76791330
Chris <32462283+chrisseymour@users.noreply.github.com>
2019-05-29 13:06:50 -0400

    Add files via upload

---
+++ Day01/hbar.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+''' hbar.py -- calculate hbar from h.
+Will state the value of hbar inside a sentence
+
+REU Student Name here
+2017-05-31
+'''
+
+# do the calculations
+h = 6.62606876e-34 
